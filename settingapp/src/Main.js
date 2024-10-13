3e8e693d3bd0923e69646dd0a68459131c0a3a22
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-10-14 00:22:44 +0530

    search bar

--- settingapp/src/Main.js
+++ settingapp/src/Main.js
@@ -1,188 +1,67 @@
-import React from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
 
-import './App.css'
 const Main = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  
+  // Define the list of settings
+  const settings = [
+    { name: 'About', path: '/about', icon: 'fa-mobile', color: 'blue' },
+    { name: 'Sim cards & mobile networks', path: '/sim', icon: 'fa-sim-card', color: 'orange' },
+    { name: 'Wifi', path: '/wifi', icon: 'fa-wifi', color: 'blue' },
+    { name: 'Bluetooth', path: '/bluetooth', icon: 'fa-bluetooth', color: 'blue' },
+    { name: 'Portable Hotspot', path: '/hotspot', icon: 'fa-link', color: 'orange' },
+    { name: 'Connection & sharing', path: '/connection', icon: 'fa-nfc-symbol', color: '#ff00008c' },
+    { name: 'Display and Brightness', path: '/display', icon: 'fa-sun', color: 'orange' },
+    { name: 'Sound and vibration', path: '/sound', icon: 'fa-volume-low', color: '#82be82' },
+  ];
+
+  // Filter settings based on the search query
+  const filteredSettings = settings.filter(setting =>
+    setting.name.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div className='main_container'>
       <div className='sub_container'>
         <h1 className='heading_primary'>Settings</h1>
         <div className='search_container'>
-          <i
-            class='fa-solid fa-magnifying-glass search_icon'
-            style={{ color: '#797b81' }}
-          ></i>
+          <i className='fa-solid fa-magnifying-glass search_icon' style={{ color: '#797b81' }}></i>
           <input
             type='text'
             className='search_text'
             placeholder='Search settings'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
         </div>
 
         <div className='apps_container'>
-          {/* <i class='fa-solid fa-mobile'></i> */}
-
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/about')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-solid fa-mobile icons'
-              style={{ background: 'blue', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>About</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/sim')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-solid fa-sim-card icons'
-              style={{ background: 'orange', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Sim cards & mobile networks</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/wifi')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-solid fa-wifi icons'
-              style={{ background: 'blue', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Wifi</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/bluetooth')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-brands fa-bluetooth icons'
-              style={{ background: 'blue', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Bluetooth</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/hotspot')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-solid fa-link icons'
-              style={{ background: 'orange', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Portable Hotspot</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/connection')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-brands fa-nfc-symbol icons'
-              style={{ background: '#ff00008c', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Connection & sharing</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/display')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-regular fa-sun icons'
-              style={{ background: 'orange ', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Display and Brightness</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-
-          <span
-            className='app_btn'
-            onClick={() => {
-              navigate('/sound')
-            }}
-            style={{ display: 'flex' }}
-          >
-            <i
-              class='fa-solid fa-volume-low icons'
-              style={{ background: '#82be82', color: 'white' }}
-            ></i>
-            <div className='app'>
-              <span>Sound and vibration</span>
-              <span>
-                {' '}
-                <i class='fa-solid fa-greater-than'></i>
-              </span>
-            </div>
-          </span>
-
-         
+          {filteredSettings.map(setting => (
+            <span
+              className='app_btn'
+              key={setting.name}
+              onClick={() => navigate(setting.path)}
+              style={{ display: 'flex' }}
+            >
+              <i
+                className={`fa-solid ${setting.icon} icons`}
+                style={{ background: setting.color, color: 'white' }}
+              ></i>
+              <div className='app'>
+                <span>{setting.name}</span>
+                <span>
+                  <i className='fa-solid fa-greater-than'></i>
+                </span>
+              </div>
+            </span>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
